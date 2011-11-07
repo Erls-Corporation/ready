00eef54a22ae1c7f4665a1e94841c8db0c64f001
Edward Hotchkiss <e@ingk.com>
2011-11-07 10:14:20 -0500

    minor note changes, done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require("colors");
 var ready = require("./lib/ready");
 
-// INIT WITH INTERVAL (BY MINUTE::5 is the default)
-ready.init("http://loft.io/", 1);
+// INIT WITH INTERVAL [5 MINUTE DEFAULT, SET FOR 30 INSTEAD]
+ready.init("http://www.some-random-new-domain.com/", 30);
 
 /* EOF */
